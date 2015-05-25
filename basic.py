4d1944280d53d4bede0dafa1e9789f64ec02b88c
Brandon DeRosier <bdero@mit.edu>
2015-05-25 00:05:19 -0400

    Add scatter for multitouch transform

--- basic.py
+++ basic.py
@@ -1,14 +1,22 @@
 from kivy.app import App
-from kivy.uix.button import Button
+
+from kivy.uix.scatter import Scatter
+from kivy.uix.label import Label
+from kivy.uix.floatlayout import FloatLayout
 
 
 class Basic(App):
     def build(self):
-        return Button(
-            text="Hello world",
-            background_color=(0, 0, 1, 1),
+        f = FloatLayout()
+        s = Scatter()
+        l = Label(
+            text="hello",
             font_size=150
         )
+
+        f.add_widget(s)
+        s.add_widget(l)
+        return f
 
 if __name__ == "__main__":
     Basic().run()
